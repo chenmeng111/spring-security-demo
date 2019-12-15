1900252416a9e4fa914ebd43ac571a1d889ed04f
chen.meng <649701771@qq.com>
2019-12-15 14:26:38 +0800

    spring security init

--- src/main/java/com/example/springsecurity/controller/TestController.java
+++ src/main/java/com/example/springsecurity/controller/TestController.java
@@ -1,11 +1,13 @@
 package com.example.springsecurity.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @RequestMapping("/test")
     @GetMapping("/test")
     public String test(){
         return "cmtest";
