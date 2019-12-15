58488842e42e2c4304c42dcbaa0cd19f85be60c3
chen.meng <649701771@qq.com>
2019-12-15 16:21:45 +0800

    add user and pwd in memory

---
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.example.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String password = passwordEncoder().encode("admin");
+        auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN");
+    }
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return super.userDetailsService();
+    }
+
+    @Override
+    public void init(WebSecurity web) throws Exception {
+        super.init(web);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.formLogin().loginPage("/login2").permitAll();
+        http
+                .authorizeRequests()
+                .antMatchers("/test").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().and()
+                .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- src/main/java/com/example/springsecurity/controller/TestController.java
+++ src/main/java/com/example/springsecurity/controller/TestController.java
@@ -1,13 +1,11 @@
 package com.example.springsecurity.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/test")
     @GetMapping("/test")
     public String test(){
         return "cmtest";
